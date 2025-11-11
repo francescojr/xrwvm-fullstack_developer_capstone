113f2e602b01fa1ec900db054964282df93f0047
jrFrancesco <francesco@hcunit.com.br>
2025-11-11 00:14:19 -0300

    Add files via upload

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,28 +1,27 @@
-"""djangoproj URL Configuration
+"""djangoproj URL Configuration"""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+    
+    # Páginas HTML estáticas (não-React)
+    path('', TemplateView.as_view(template_name="Home.html"), name='home'),
+    path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
+    path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
+    
+    # Rotas React - AMBAS servem o MESMO index.html
+    # O React Router (App.js) decide qual componente mostrar
+    path('login/', TemplateView.as_view(template_name="index.html"), name='login_page'),
+    path('register/', TemplateView.as_view(template_name="index.html"), name='register_page'),  # ✅ MESMO index.html
+    
+    # API endpoints
     path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
