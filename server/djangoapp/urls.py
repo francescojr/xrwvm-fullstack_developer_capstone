15fa1a7f1020758d43c36d4332f7784be770d73a
jrFrancesco <francesco@hcunit.com.br>
2025-11-11 19:38:26 -0300

    microservices

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,7 +16,11 @@ urlpatterns = [
     path('get_current_user/', views.get_current_user, name='get_current_user'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     path('populate/', views.populate_database, name='populate_database'),
-
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
     # path('register/', TemplateView.as_view(template_name="index.html")),
     
     # Outras rotas (implementar depois)
