7ae8f61fb6d12725f0f12c0e4b45c1213445ad2d
jrFrancesco <francesco@hcunit.com.br>
2025-11-11 00:11:17 -0300

    Add files via upload

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,23 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-# from . import views
+from django.urls import path
+from . import views
+
+from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Página principal
+    path('', views.get_dealerships, name='index'),
+    
+    # API de autenticação
+    path('login/', views.login_user, name='login_api'),
+    path('logout/', views.logout_request, name='logout_api'),
+    path('register/', views.registration, name='register_api'),  # ✅ Rota de registro
+    path('get_current_user/', views.get_current_user, name='get_current_user'),
+    # path('register/', TemplateView.as_view(template_name="index.html")),
+    
+    # Outras rotas (implementar depois)
+    # path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    # path('dealer/<int:dealer_id>/reviews/', views.get_dealer_reviews, name='dealer_reviews'),
+    # path('add_review/', views.add_review, name='add_review'),
+]
