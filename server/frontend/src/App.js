6a8902d0db131fa1d41d7dd38bb656fbfa06bf71
jrFrancesco <francesco@hcunit.com.br>
2025-11-11 00:13:37 -0300

    Add files via upload

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,19 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  
+  console.log('Current path:', location.pathname);
+  
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<div>404 - Page not found. Path: {location.pathname}</div>} />
     </Routes>
   );
 }
+
 export default App;
