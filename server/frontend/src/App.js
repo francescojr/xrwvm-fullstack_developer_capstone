717a03a522d60dbdf54c5945075cd1b9fff2aa92
jrFrancesco <francesco@hcunit.com.br>
2025-11-11 22:17:11 -0300

    Add files via upload

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,10 @@
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import { Routes, Route, useLocation } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from './components/Dealers/Dealer';
+import PostReview from "./components/Dealers/PostReview";
+
 
 function App() {
   const location = useLocation();
@@ -11,6 +15,9 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
       <Route path="*" element={<div>404 - Page not found. Path: {location.pathname}</div>} />
     </Routes>
   );
